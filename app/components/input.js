0de5071ddbb5078eb9a7f1b89b75a27c65d596e8
Jack Torrealba <jmarcus1165@gmail.com>
2025-12-13 17:28:32 -0500

    fixed issues caused by library updates

--- app/components/input.js
+++ app/components/input.js
@@ -1,21 +1,10 @@
-import { ArrowForwardIcon } from "@chakra-ui/icons"
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Center, Box, RadioCard, Input, Button, Field, Text, FormControl, Heading, FormLabel, RadioGroup, HStack,Radio, FormErrorMessage, Container } from "@chakra-ui/react";
-import theme from './theme';
-import { CircularProgress } from "@chakra-ui/react";
+import { Center, Box, RadioCard, Span, Input, Button, Field, Text, HStack, FieldHelperText } from "@chakra-ui/react";
+import { ProgressCircle } from "@chakra-ui/react";
 import html2canvas from "html2canvas";
 
-
-
-export default function InputProfile(){
-
-  // define useStates
-  const [pageInfo, setPageInfo] = useState(null)
-  const [profileLink, setProfileLink] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [currentDate, setCurrentDate] = useState('')
-  const [showButton, setShowButton] = useState(false)
 
   // define colors for background and text
   const backgroundColors = [
@@ -32,9 +21,24 @@ export default function InputProfile(){
     { value : "#3a6f73"},
   ]
 
+
+export default function InputProfile() {
+  // define useStates
+  const [pageInfo, setPageInfo] = useState(null)
+  const [displayName, setDisplayName] = useState('')
+  const [profileLink, setProfileLink] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [currentDate, setCurrentDate] = useState('')
+  const [showButton, setShowButton] = useState(false)
+  const [showSubmitButton, setShowSubmitButton] = useState(true);
+
   // get the profile link from the input
   const handleInputChange = (e) => {
     setProfileLink(e.target.value);
+  };
+
+  const getDisplayName = (e) => {
+    setDisplayName(e.target.value)
   };
 
   // validate the url that the user inputted
@@ -42,6 +46,7 @@ export default function InputProfile(){
     const urlRegex = /^https:\/\/www\.goodreads\.com\/user\/show\/\d+$/;
     return urlRegex.test(profileLink);
   };
+
 
   // get the current date in specific format (ex: 15 January 2024) (this is for the output png result)
   useEffect(() => {
@@ -57,7 +62,8 @@ export default function InputProfile(){
   const handleSubmit = async (e) => {
 
     e.preventDefault();
-    setLoading(true); // show the loading icon
+    setLoading(true) // show the loading icon
+    setShowSubmitButton(false)
     setShowButton(false) // hide the download png result button
 
     // initialize username string
@@ -133,124 +139,129 @@ export default function InputProfile(){
     }
   };
 
+  function LoadingIcon({isLoading}) {
+    if (isLoading) {
+      return (
+        <Center id="loadingIconBox" mt='2rem'>
+          <ProgressCircle.Root value={null}>
+            <ProgressCircle.Circle>
+              <ProgressCircle.Track/>
+              <ProgressCircle.Range strokeLinecap="round" stroke="customYellow"/>
+            </ProgressCircle.Circle>
+          </ProgressCircle.Root>
+      </Center>
+      )
+    }
+  }
+
   
     
   return (
     <>
-      
-        <Box w="50%" mx="auto" mt="2rem">
-          <Field.Root required>
-            <Field.Label>
-              Your Name <Field.RequiredIndicator />
-            </Field.Label>
-            <Input />
-          </Field.Root>
-          <RadioCard.Root  my="1rem">
-            <RadioCard.Label>Pick a background color</RadioCard.Label>
-            <HStack align="stretch">
-              {backgroundColors.map((item) => (
-                <RadioCard.Item key={item.value} value={item.value} background={item.value} h="40px">
-                  <RadioCard.ItemHiddenInput />
-                  <RadioCard.ItemControl/>
-                </RadioCard.Item>
-              ))}
-            </HStack>
-          </RadioCard.Root>
-          <RadioCard.Root  my="1rem">
-            <RadioCard.Label>Pick a font color</RadioCard.Label>
-            <HStack align="stretch">
-              {fontColors.map((item) => (
-                <RadioCard.Item key={item.value} value={item.value} background={item.value} h="40px">
-                  <RadioCard.ItemHiddenInput />
-                  <RadioCard.ItemControl/>
-                </RadioCard.Item>
-              ))}
-            </HStack>
-          </RadioCard.Root>
-        </Box>
-        <Box id='inputBox' display='flex' justifyContent='center' alignItems='center' mt='3rem'>
-          <Box className="inputBox" width='50%'>
-            <Input
-              id="submitInput"
-              onChange={handleInputChange} 
-              variant='outline'
-              placeholder="https://www.goodreads.com/user/show/123456789"
-              _placeholder={{color: 'offWhite'}}
-              bg='customBlue'
-              border='none'
-              fontFamily='Karla, sans-serif'
-            /> 
-          </Box> 
-          <Box className="btnBox" pl='1rem'>
-            <Button id="submitBtn" onClick={handleSubmit} bg='customBlue' color='offWhite' isDisabled={!isUrlValid()}> 
-              <ArrowForwardIcon/> 
+      <Box id="formBox" w="50%" mx="auto" mt="2rem">
+        <Field.Root required>
+          <Field.Label>
+            Your Name <Field.RequiredIndicator />
+          </Field.Label>
+          <Input onChange={getDisplayName} bg="customBlue" p="8px" />
+        </Field.Root>
+        <RadioCard.Root  my="1rem">
+          <RadioCard.Label>Pick a background color:</RadioCard.Label>
+          <HStack align="stretch">
+            {backgroundColors.map((item) => (
+              <RadioCard.Item key={item.value} value={item.value} background={item.value} h="40px">
+                <RadioCard.ItemHiddenInput />
+                <RadioCard.ItemControl/>
+              </RadioCard.Item>
+            ))}
+          </HStack>
+        </RadioCard.Root>
+        <RadioCard.Root  my="1rem">
+          <RadioCard.Label>Pick a font color:</RadioCard.Label>
+          <HStack align="stretch">
+            {fontColors.map((item) => (
+              <RadioCard.Item key={item.value} value={item.value} background={item.value} h="40px">
+                <RadioCard.ItemHiddenInput />
+                <RadioCard.ItemControl/>
+              </RadioCard.Item>
+            ))}
+          </HStack>
+        </RadioCard.Root>
+        <Field.Root required>
+          <Field.Label>
+            Goodreads Profile Link <Field.RequiredIndicator />
+          </Field.Label>
+          <Input id="submitInput" onChange={handleInputChange} bg="customBlue" p="8px"/>
+          <FieldHelperText color="black" >
+            Your profile link should look like: <Span whiteSpace="pre" className='homeTextLink' color="customYellow">https://www.goodreads.com/user/show/123456789</Span>
+          </FieldHelperText>
+        </Field.Root>
+        <Center>
+          {showSubmitButton ? (
+            <Button my="1.5rem" p="8px" id="submitBtn" onClick={handleSubmit} bg='customBlue' color='offWhite' disabled={!isUrlValid()}> 
+              Get Reading Wrapped
             </Button>
-          </Box>
-        </Box>
+          ): <Box></Box>}
+        </Center>
+      </Box>
         
+      {loading ? <LoadingIcon isLoading={true}/> : <Box></Box>}
+
+      <Center id="downloadBtnBox" mt='2rem'>
+        {showButton ? <Button my="1.5rem" p="8px" w="25%" bg="customYellow" onClick={handleDownloadImage}>Download PNG</Button>: null}
+      </Center>
+
+      {pageInfo ? (
+        
+        <Box fontFamily='Noto Serif, serif' className="result" id="result" display='none' >
+
+          <Box textAlign='right' pr='5rem'>
+            <Text fontSize='49px'>{pageInfo.username}&apos;s Read Receipt</Text>
+            <Text mt='-0.5rem' fontSize='30px'>{currentDate}</Text>
+          </Box>
+
+          <Center mt='8rem'>
+            <Text fontSize='40px'>Based on {pageInfo.totalBooks} books for the past year, you are the</Text>
+          </Center>
+
+          <Center>
+            <Text fontSize='120px'>{pageInfo.firstWord}</Text>
+          </Center>
+
+          <Center mt='-4.5rem'>
+            <Text fontSize='120px'>{pageInfo.secondWord}</Text>
+          </Center>
+
+          <Box width='100%' mt='8rem'>
+            <Box textAlign='center' ml='600px'>
+              <Text fontSize='30px'>Average popularity of books read:</Text>
+              <Text mt='-1rem' fontSize='50px'>{Math.round(pageInfo.avgRatings).toLocaleString()} ratings</Text>
+            </Box>
+          </Box>
+
+          <Box width='100%'>
+            <Box textAlign='center' mr='650px'>
+              <Text textAlign='center' fontSize='30px'>Most read genre:</Text>
+              <Text mt='-1.5rem' textAlign='center' fontSize='50px'>{pageInfo.topGenreRead}</Text>
+            </Box>
+          </Box>
+
+          <Center mt='30rem'>
+            <Text lineHeight='1' width='80%' textAlign='center' fontSize='35px'>{pageInfo.firstDesc}</Text>
+          </Center>
           
-        <Center id="loadingIconBox" mt='2rem'>
-          {loading ? <CircularProgress isIndeterminate color="customYellow"></CircularProgress>: null}
-        </Center>
+          <Center mt='2rem'>
+            <Text lineHeight='1' width='80%' textAlign='center' fontSize='35px'>{pageInfo.secondDesc}</Text>
+          </Center>
 
-        <Center id="downloadBtnBox" mt='2rem'>
-          {showButton ? <Button bg="customYellow" onClick={handleDownloadImage}>Download PNG</Button>: null}
-        </Center>
+          <Center className="url" fontSize='25px' mt='8rem'>
+            <Text >read-receipts.com</Text>
+          </Center>
 
-        {pageInfo ? (
-
-          <Box fontFamily='Noto Serif, serif' className="result" id="result" display='none' >
-
-            <Box textAlign='right' pr='5rem'>
-              <Text fontSize='49px'>{pageInfo.username}&apos;s Read Receipt</Text>
-              <Text mt='-0.5rem' fontSize='30px'>{currentDate}</Text>
-            </Box>
-
-            <Center mt='8rem'>
-              <Text fontSize='40px'>Based on {pageInfo.totalBooks} books for the past year, you are the</Text>
-            </Center>
-
-            <Center>
-              <Text fontSize='120px'>{pageInfo.firstWord}</Text>
-            </Center>
-
-            <Center mt='-4.5rem'>
-              <Text fontSize='120px'>{pageInfo.secondWord}</Text>
-            </Center>
-
-            <Box width='100%' mt='8rem'>
-              <Box textAlign='center' ml='600px'>
-                <Text fontSize='30px'>Average popularity of books read:</Text>
-                <Text mt='-1rem' fontSize='50px'>{Math.round(pageInfo.avgRatings).toLocaleString()} ratings</Text>
-              </Box>
-            </Box>
-
-            <Box width='100%'>
-              <Box textAlign='center' mr='650px'>
-                <Text textAlign='center' fontSize='30px'>Most read genre:</Text>
-                <Text mt='-1.5rem' textAlign='center' fontSize='50px'>{pageInfo.topGenreRead}</Text>
-              </Box>
-            </Box>
-
-            <Center mt='30rem'>
-              <Text lineHeight='1' width='80%' textAlign='center' fontSize='35px'>{pageInfo.firstDesc}</Text>
-            </Center>
-            
-            <Center mt='2rem'>
-              <Text lineHeight='1' width='80%' textAlign='center' fontSize='35px'>{pageInfo.secondDesc}</Text>
-            </Center>
-
-            <Center className="url" fontSize='25px' mt='8rem'>
-              <Text >read-receipts.com</Text>
-            </Center>
-
-          </Box>
-        ) : (
-          <Box></Box>
-        )}
-        
-      
+        </Box>
+      ) : (
+        <Box></Box>
+      )}
     </>
-  
   )
 }
--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 import { ChakraProvider, Box, defaultSystem} from "@chakra-ui/react"
 import Title from './components/title';
 import TitleMessage from './components/titleMessage';
-import HelperText from './components/helperText';
 import InputProfile from './components/input';
 import LinkToForm from './components/linkToForm';
 import theme from "./components/theme";
@@ -13,11 +12,12 @@ export default function Home() {
       
     <ChakraProvider value={theme}>
       <Box className="homePage">
-        <Title></Title>
-        <TitleMessage></TitleMessage>
-        <InputProfile></InputProfile>
-        <HelperText></HelperText>
-        {/* <LinkToForm></LinkToForm> */}
+        <Box pb="25%">
+          <Title></Title>
+          <TitleMessage></TitleMessage>
+          <InputProfile></InputProfile>
+          <LinkToForm></LinkToForm>
+        </Box>
       </Box>
     </ChakraProvider >
         
